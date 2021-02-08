4e43de0e67a24017ca7b72221cfa1d5c3ca617e5
wjm3824 <wjm3824@rit.edu>
2021-02-08 15:56:39 -0700

    Added a simple layout for the BSP

--- AtlasEngine/BSP.cpp
+++ AtlasEngine/BSP.cpp
@@ -1,10 +1,66 @@
 #include "pch.h"
 #include "BSP.h"
 
-void Init_BSP()
+void Fill_BSP(BSP* bsp, Instance* inst, int instanceCount = 1)
 {
+	if (!bsp->initialized) return;
+
+	//TODO Add the instances to the bsp, do nothing else
+}
+
+BSP* Generate_BSP(BSP* bsp)
+{
+	if (!bsp->initialized) return;
+
+	//TODO Take all other instances below this node and add it to this one, then delete all child nodes
+
+	//TODO split this node into children as neccessary
+}
+
+void Init_BSP(BSP* bsp, int maxSubdivisions, int minToSubdivide)
+{
+	bsp->maxSubdivisions = maxSubdivisions;
+	bsp->minToSubdivide = minToSubdivide;
+	bsp->subdivisionLevel = 0;
+	bsp->split = false;
+	bsp->numInstances = 0;
+	bsp->meanContentsValue = 0.0f;
+
+	bsp->boundingBox.back = 0.0f;
+	bsp->boundingBox.front = 0.0f;
+	bsp->boundingBox.left = 0.0f;
+	bsp->boundingBox.right = 0.0f;
+	bsp->boundingBox.top = 0.0f;
+	bsp->boundingBox.bottom = 0.0f;
+
+	bsp->initialized = true;
+
+	//TODO Remove children and parents if there are any
+}
+
+void Place_Into_BSP(BSP* bsp, Instance* inst)
+{
+	if (!bsp->initialized) return;
+
+	//TODO If this is the head node and the instance is outside of its bounds, we have to regenerate everything
 
 
+
+	//TODO if there are child nodes we will put this instance in one (or both???) of them (call pib and quit out)
+
+
+
+	//TODO else If the current node does not have children, add the instance, increment the meanContentsValue, and check if a split is neccessary
+
+
+
+}
+
+void Clear_BSP_Node(BSP* bsp)
+{
+	if (!bsp->initialized) return;
+
+	//Remove all instances from this node downwards, then delete all of this node's children
 
 }
 
--- AtlasEngine/BSP.h
+++ AtlasEngine/BSP.h
@@ -10,32 +10,49 @@ struct BoundingBox
 	float back;
 };
 
+enum BSPSplitDirection
+{
+	leftRight,
+	frontBack,
+	topBottom
+};
+
 struct BSP
 {
+	bool initialized = false;
+
+	int subdivisionLevel;
+
 	int maxSubdivisions;
 	int minToSubdivide;
+
+	bool split;
 
 	int numInstances;
 	Instance* instances;
 
-	int numChildren;
+	BSP* root;
+	BSP* parent;
 	BSP* children;
 
-	BSP* parent;
-
 	BoundingBox boundingBox;
-
-	//array of booleans top / left / front
-
-
+	float meanContentsValue; //determines where the BSP should split (-1 to 1)
 };
 
-//Root node initialization
-void Init_BSP(BoundingBox boundingBox, int maxSubdivisions, int minToSubdivide);
+//Add instances that will be split into
+//BSP nodes when 'Generate_BSP' is called
+void Fill_BSP(BSP* bsp, Instance* inst, int instanceCount = 1);
 
-//Check if it should subdivide
-//-> left/right first
+//Creates a BSP tree from this node downward
+BSP* Generate_BSP(BSP* bsp);
 
-//
-void Subdivide();
+//Deletes all of the data in this node and all nodes below
+void Clear_BSP_Node(BSP* bsp);
+
+//Sets up a BSP node to be a head node
+void Init_BSP(BSP* bsp, int maxSubdivisions, int minToSubdivide);
+
+//Call this to put an instance in an
+//already created BSP tree
+void Place_Into_BSP(BSP* bsp, Instance* inst);
 
